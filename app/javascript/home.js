79d53d99a35944c510e893f41f0737df92f9f339
HamazakiHisanori <hiii1023@icloud.com>
2021-07-09 06:28:19 +0900

    トップページのディスプレイ

---
+++ app/javascript/home.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+  const text = document.getElementById("text");
+    text.addEventListener('click', function() {
+      text.style.border="solid red";
+  });
+});
