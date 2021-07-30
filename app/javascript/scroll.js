e4e202c043020ce129d0b6e3d54617ab2e491ad2
HamazakiHisanori <hiii1023@icloud.com>
2021-07-30 16:33:51 +0900

    スクロールイベントの追加

---
+++ app/javascript/scroll.js
@@ -0,0 +1,33 @@
+$(function() {
+  const top = $('#top').offset().top;
+  const news = $('#news').offset().top - 70;
+  const menu = $('#menu').offset().top- 70;
+  const contact = $('#contact').offset().top- 70;
+  const access = $('#access').offset().top- 70;
+
+  const speed = 800;
+
+  $('#top-link').click(function() {
+    $("html,body").animate({scrollTop:top},speed);
+  });
+
+  $('#news-link').click(function() {
+    $("html,body").animate({scrollTop:news},speed);
+  });
+
+  $('#menu-link').click(function() {
+    $("html,body").animate({scrollTop:menu},speed);
+  });
+
+  $('#contact-link').click(function() {
+    $("html,body").animate({scrollTop:contact},speed);
+  });
+
+  $('#access-link').click(function() {
+    $("html,body").animate({scrollTop:access},speed);
+  });
+
+});
+
+
+
