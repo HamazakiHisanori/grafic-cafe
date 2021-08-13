db940a4dc509dcd512041ff3e3613d0452f752fe
HamazakiHisanori <hiii1023@icloud.com>
2021-08-13 15:05:41 +0900

    er図作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :menus
+  has_many :news
 
   with_options presence: { message: '' } do
     validates :name, length: { maximum: 10, message: '' }
